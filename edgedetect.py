8b404d2eeec471d65a48beac93626072e76c94ad
Zarkonnen <david.stark@zarkonnen.com>
2016-12-03 14:31:48 +0100

    Reasonably reliable coin detection.

--- edgedetect.py
+++ edgedetect.py
@@ -9,11 +9,13 @@ from scipy.ndimage.filters import convolve
 from skimage.transform import hough_circle
 from skimage.draw import circle_perimeter
 
-image = rescale(imread("data/photos/DSC08321.jpg", as_grey=True), 0.5)
+original_image = imread("data/photos/DSC08320.jpg", as_grey=True)
+
+image = rescale(original_image, 1200.0 / len(image))
 
 edges = canny(image, sigma=0.7)
 
-kernel = np.array(7 * [7*[1]])
+kernel = np.array(5 * [5*[1]])
 
 edges = convolve(edges, kernel)
 
@@ -21,7 +23,7 @@ fill = ndi.binary_fill_holes(edges)
 
 edges = canny(fill)
 
-hough_radii = np.arange(50, 150, 2)
+hough_radii = np.arange(50, 150, 5)
 hough_res = hough_circle(edges, hough_radii)
 
 centers = []
@@ -41,7 +43,7 @@ for idx in np.argsort(accums)[::-1][:100]:
     radius = radii[idx]
     add = True
     for c in circles:
-        if (c[0] - center_y) * (c[0] - center_y) + (c[1] - center_x) * (c[1] - center_x) < (c[2] + radius) * (c[2] + radius) * 0.7:
+        if (c[0] - center_y) * (c[0] - center_y) + (c[1] - center_x) * (c[1] - center_x) < (c[2] + radius) * (c[2] + radius) * 0.25:
             add = False
             break
     if add:
@@ -55,5 +57,5 @@ for circle in circles:
     cx, cy = circle_perimeter(*circle)
     image[cy, cx] = (220, 20, 20)
 
-ve = ImageViewer(image)#np.sum(hough_res, axis=0))
+ve = ImageViewer(image)#np.sum([image, np.sum(hough_res / 10., axis=0)], axis=0))
 ve.show()
