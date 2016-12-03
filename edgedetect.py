79f9e0a5410e07a3d900dfde45824101d4e7b88e
Zarkonnen <david.stark@zarkonnen.com>
2016-12-03 13:09:59 +0100

    Using convolution to enable fill to work.

--- edgedetect.py
+++ edgedetect.py
@@ -1,20 +1,20 @@
+import numpy as np
 from skimage.io import imread
 from skimage.transform import rescale
 from skimage.feature import canny
 from scipy import ndimage as ndi
 from skimage.viewer import ImageViewer
+from scipy.ndimage.filters import convolve
 
-image = rescale(imread("data/photos/DSC08315.jpg", as_grey=True), 0.1)
+image = rescale(imread("data/photos/DSC08315.jpg", as_grey=True), 0.5)
 
-edges = canny(image)
+edges = canny(image, sigma=0.7)
+
+kernel = np.array(5 * [5*[1]])
+
+edges = convolve(edges, kernel)
 
 fill = ndi.binary_fill_holes(edges)
 
-vi = ImageViewer(image)
-vi.show()
-
-ve = ImageViewer(edges)
+ve = ImageViewer(fill)
 ve.show()
-
-vf = ImageViewer(fill)
-vf.show()
